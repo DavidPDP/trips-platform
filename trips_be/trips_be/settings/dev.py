643ab5f2bdd4c9806634456ecc075dd7cab1de67
DavidPDP <ballesto_@hotmail.com>
2020-11-16 11:31:29 -0500

    fix static files

--- trips_be/trips_be/settings/dev.py
+++ trips_be/trips_be/settings/dev.py
@@ -47,6 +47,22 @@ CORS_ALLOW_CREDENTIALS = True
 
 WSGI_APPLICATION = 'trips_be.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
